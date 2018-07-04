9ccb9e6fd779b3cbf6a9afdc87bcc39a968394bb
RudiHansen <rsh@pobox.com>
2018-07-04 21:01:06 +0200

    More test Picker binding

--- TestXamarin.Forms/TestXamarin.Forms/Views/ItemDetailPage.xaml.cs
+++ TestXamarin.Forms/TestXamarin.Forms/Views/ItemDetailPage.xaml.cs
@@ -5,6 +5,7 @@ using Xamarin.Forms.Xaml;
 
 using TestXamarin.Forms.Models;
 using TestXamarin.Forms.ViewModels;
+using System.Collections.Generic;
 
 namespace TestXamarin.Forms.Views
 {
@@ -14,13 +15,25 @@ namespace TestXamarin.Forms.Views
 	{
         public string[] users = { "Tim", "Dan", "Christa" };
 
+        public class User
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+
+        public List<User> userList = new List<User>();
+
         ItemDetailViewModel viewModel;
+
 
         public ItemDetailPage(ItemDetailViewModel viewModel)
         {
             InitializeComponent();
 
             BindingContext = this.viewModel = viewModel;
+            userList.Add(new User { Name = "Tim", Description = "Tim Parson" });
+            userList.Add(new User { Name = "Dan", Description = "Dan Parson" });
+            userList.Add(new User { Name = "Christa", Description = "Christa Parson" });
         }
 
         public ItemDetailPage()
